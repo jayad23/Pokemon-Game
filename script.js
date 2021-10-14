a1c44b0daaa4e67e5e000c6f709d5781bba884a7
jayad23 <jaidervanegas@live.com>
2021-10-14 13:24:15 -0500

    We have added the button start prior the game

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
-
 window.addEventListener('DOMContentLoaded', e =>{
-    const character = document.getElementById('character')
-    const block = document.getElementById('block')
-    const game = document.getElementById('game')
+    
+    const startDiv = document.getElementById('start')
+    const startButton = document.getElementById('start')
+    startButton.addEventListener('click', (e) => clicked(e))
 
-    game.addEventListener('click', (e) => jump(e))
-
-
-    function jump (event){
-        if(character.classList != 'animate'){
-            character.classList.toggle('animate')
-        }
-        setTimeout(function (){
-            character.classList.toggle('animate')
-        }, 500)
+    function clicked (event){
+        startDiv.style.display = "none"
+        game.style.visibility = "visible"
+        gamePoke()
+        block.classList = ('animateBall')
     }
 
-    const checkDead = setInterval(() => {
-        let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
-        let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
-        if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
+    function gamePoke (){
+        const character = document.getElementById('character')
+        const block = document.getElementById('block')
+        const game = document.getElementById('game')
+
+        game.addEventListener('click', (e) => jump(e))
+
+
+        function jump (event){
+            if(character.classList != 'animate'){
+            character.classList.toggle('animate')
+            }
+            setTimeout(function (){
+            character.classList.toggle('animate')
+            }, 500)
+        }
+
+        const checkDead = setInterval(() => {
+            let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"))
+            let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"))
+            if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130){
             block.style.animation = "none"
             block.style.display = "none"
             alert("U just Lose! ;) Atte: Kike")
-        }
-    },10);
+            }
+        },10);
+    }
+    
+    
+
 })
 
